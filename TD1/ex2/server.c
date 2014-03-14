a197beac647b08e44ff4047a4ffffa6171bff7fd
SteinX <xiayiming007@gmail.com>
2014-03-14 22:17:03 +0100

    Pas sur, reste a amerliorer

--- TD1/ex2/server.c
+++ TD1/ex2/server.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <signal.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
 #define IPC_KEY				0x55
 #define NB_MAX_CLT 			4
-#define NB_MAX_CLT_SIM		2
 #define NB_MAX_TYP_OBJ		3
 
 #define REQ_SRV	 			1
@@ -25,6 +26,7 @@
 #define REP_CREATION_PANIER	202
 #define REP_LISTE_OBJETS	203
 #define REP_INFO_OBJ        204
+#define REP_SERVER_FULL		205
 
 #define OBJET_UN			"Pomme"
 #define OBJET_DEUX			"Banane"
@@ -46,8 +48,11 @@ typedef struct
 	objet 	panier[NB_MAX_TYP_OBJ];
 } msg;
 
+/*Global variables*/
 int id_msg;
 static objet stockage[NB_MAX_TYP_OBJ];
+static bool  sousProcessusExiste = false;
+const int MSG_SIZE = sizeof(msg) - sizeof(long);
 
 void sigintHandler(){
 	msgctl(id_msg,IPC_RMID,0);
@@ -55,17 +60,112 @@ void sigintHandler(){
 	exit(0);
 }
 
+void sousProcessusCompeleteHandler(){
+	sousProcessusExiste = false;
+}
+
+void traiteMessage(msg *message, int *nbClient, objet *clientPanier, int *num_client, bool isSousProcessus){
+	char tmpLog[255];
+	switch(message->req)
+	{
+		case DEM_NUM_CLT:
+			puts("cli_noid ---[demande numéro client]---> serveur     		 DEM_NUM_CLT");
+			if (*nbClient == NB_MAX_CLT)
+			{
+				message->type = REP_CLT_PLEIN;
+				message->num_clt = -1;
+				msgsnd(id_msg, message, MSG_SIZE, 0);
+				break;
+			}
+			message->type = REP_NUM_CLT;
+            //enlever le rand pour eviter 2 clients avec le même
+            //numéro
+			message->num_clt = *num_client;
+			(*num_client) = (*num_client % 4) + 1;
+			msgsnd(id_msg, message, MSG_SIZE, 0);
+			puts("cli_noid <---[fournir numéro client]--- serveur          REP_NUM_CLT\n");
+			(*nbClient)++;
+			break;
+		case DEM_CREATION_PANIER:						
+			sprintf(tmpLog, "cli_%d ---[Demande de creation de panier]---> serveur       DEM_CREATION_PANIER", message->num_clt);
+			puts(tmpLog);
+			memset(tmpLog, 0, sizeof(tmpLog));
+
+			message->type = message->num_clt;
+			message->req = REP_CREATION_PANIER;
+			memcpy(clientPanier, stockage, sizeof(stockage));
+			
+			int i = 0;
+			for(;i < NB_MAX_TYP_OBJ;i++)
+				clientPanier[i].nb = 0;
+            
+            memcpy(message->panier, clientPanier,
+                    sizeof(message->panier));
+			message->ret = 0;
+			msgsnd(id_msg, message, MSG_SIZE, 0);
+			
+			sprintf(tmpLog, "cli_%d <---[Creation de panier]--- serveur       REP_CREATION_PANIER\n", message->num_clt);
+			puts(tmpLog);
+			break;
+		case DEM_LISTE_OBJETS:
+			sprintf(tmpLog, "cli_%d ---[Demande la liste d'objet]---> serveur       DEM_LISTE_OBJETS", message->num_clt);
+			puts(tmpLog);
+			memset(tmpLog, 0, sizeof(tmpLog));
+
+			message->type = message->num_clt;
+			message->req = REP_LISTE_OBJETS;
+			memcpy(message->panier, stockage, sizeof(stockage));
+			for(;i < NB_MAX_TYP_OBJ;i++)
+				message->panier[i].nb = 0;
+			
+			message->ret = 0;
+			msgsnd(id_msg, message, MSG_SIZE, 0);
+
+			sprintf(tmpLog, "cli_%d <---[Fournir liste d'objet]--- serveur       REP_LISTE_OBJETS\n", message->num_clt);
+			puts(tmpLog);
+			break;
+        case DEM_INFO_OBJ:
+            sprintf(tmpLog, "cli_%d ---[Demande du stock et du prix]--> serveur    DEM_INFO_OBJ\n", message->num_clt);
+            puts(tmpLog);
+            memset(tmpLog, 0, sizeof(tmpLog));
+
+            message->type = message->num_clt;
+            message->req = REP_INFO_OBJ;
+            memcpy(message->panier, stockage, 
+                    sizeof(message->panier));
+
+            message->ret = 0;
+            msgsnd(id_msg, message, MSG_SIZE, 0);
+
+            sprintf(tmpLog, "cli_%d <---[Fournir le stock et prix]--- REP_INFO_OBJ\n",
+                    message->num_clt);
+            puts(tmpLog);
+            break;
+	}
+	if (isSousProcessus)
+	{
+		kill(getppid(), SIGUSR1);
+	}else{
+		puts("Main over and sleep");
+		sleep(20);
+		puts("Main awake.");
+	}
+}
+
 int main()
 {
 	srand(time(0));
-	const int MSG_SIZE = sizeof(msg) - sizeof(long);
 
+	pid_t sp;
 	key_t cle;
 	int nb_client;
 	objet clientPanier[NB_MAX_TYP_OBJ];
 	msg message;
+	//Pour savoir si le serveur est en train de traiter le meme client en 2 ligne
+	bool clientTable[NB_MAX_CLT + 1] = {false, false, false, false, false};
+
     //numero client a retourner au client
-    int num_client = 0;
+    int num_client = 1;
 	cle = ftok("sr03p012", IPC_KEY);
 
 	//init stockage
@@ -77,6 +177,7 @@ int main()
 	stockage[2] = tmpObjet3;
 
 	signal(SIGINT, sigintHandler);
+	signal(SIGUSR1, sousProcessusCompeleteHandler);
 	
 	if(cle == -1)
 	{
@@ -92,83 +193,47 @@ int main()
 		else
 		{
 			while(1){
-				char tmpLog[255];
-				msgrcv(id_msg, (void*)&message, MSG_SIZE, REQ_SRV, 0);
-				switch(message.req)
+				msgrcv(id_msg, &message, MSG_SIZE, REQ_SRV, 0);
+				if (!clientTable[message.num_clt])
 				{
-					case DEM_NUM_CLT:
-						puts("cli_noid ---[demande numéro client]---> serveur     		 DEM_NUM_CLT");
-						if (nb_client == NB_MAX_CLT)
+					clientTable[message.num_clt] = true;
+				}else{
+					message.type = message.num_clt;
+					message.req = REP_SERVER_FULL;
+					message.ret = -1;
+					msgsnd(id_msg, &message, MSG_SIZE, 0);
+					continue;
+				}
+
+				if(!sousProcessusExiste){
+					sp = fork();
+					if(sp == -1){
+						perror("Sous processus");
+						exit(1);
+					}
+					if(sp == 0){
+						msgrcv(id_msg, &message, MSG_SIZE, REQ_SRV, 0);
+						puts("Sub Task:");
+						traiteMessage(&message, &nb_client, clientPanier, &num_client, true);
+						puts("Sub over.");	
+						if (message.type == REP_NUM_CLT)
 						{
-							message.type = REP_CLT_PLEIN;
-							message.num_clt = -1;
-							msgsnd(id_msg, &message, MSG_SIZE, 0);
-							break;
+							clientTable[0] = false;
+						} else{
+							clientTable[message.num_clt] = false;
 						}
-						message.type = REP_NUM_CLT;
-                        //enlever le rand pour eviter 2 clients avec le même
-                        //numéro
-						message.num_clt = num_client++;
-						msgsnd(id_msg, (void*)&message, MSG_SIZE, 0);
-						puts("cli_noid <---[fournir numéro client]--- serveur          REP_NUM_CLT\n");
-						nb_client++;
-						break;
-					case DEM_CREATION_PANIER:						
-						sprintf(tmpLog, "cli_%d ---[Demande de creation de panier]---> serveur       DEM_CREATION_PANIER", message.num_clt);
-						puts(tmpLog);
-						memset(tmpLog, 0, sizeof(tmpLog));
+						return 0;
+					}
+				}
 
-						message.type = message.num_clt;
-						message.req = REP_CREATION_PANIER;
-						tmpObjet1.nb = 0;
-						tmpObjet2.nb = 0;
-						tmpObjet3.nb = 0;
-						clientPanier[0] = tmpObjet1;
-						clientPanier[1] = tmpObjet2;
-						clientPanier[2] = tmpObjet3;
-                        memcpy(message.panier, clientPanier,
-                                sizeof(message.panier));
-						message.ret = 0;
-						msgsnd(id_msg, (void*)&message, MSG_SIZE, 0);
-						
-						sprintf(tmpLog, "cli_%d <---[Creation de panier]--- serveur       REP_CREATION_PANIER\n", message.num_clt);
-						puts(tmpLog);
-						memset(tmpLog, 0, sizeof(tmpLog));
-						break;
-					case DEM_LISTE_OBJETS:
-						sprintf(tmpLog, "cli_%d ---[Demande la liste d'objet]---> serveur       DEM_LISTE_OBJETS", message.num_clt);
-						puts(tmpLog);
-						memset(tmpLog, 0, sizeof(tmpLog));
-
-						message.type = message.num_clt;
-						message.req = REP_LISTE_OBJETS;
-						memcpy(message.panier, clientPanier, sizeof(message.panier));
-						
-						message.ret = 0;
-						msgsnd(id_msg, (void *)&message, MSG_SIZE, 0);
-
-						sprintf(tmpLog, "cli_%d <---[Fournir liste d'objet]--- serveur       REP_LISTE_OBJETS\n", message.num_clt);
-						puts(tmpLog);
-						memset(tmpLog, 0, sizeof(tmpLog));
-						break;
-                    case DEM_INFO_OBJ:
-                        sprintf(tmpLog, "cli_%d ---[Demande du stock et du prix]--> serveur    DEM_INFO_OBJ\n", message.num_clt);
-                        puts(tmpLog);
-                        memset(tmpLog, 0, sizeof(tmpLog));
-
-                        message.type = message.num_clt;
-                        message.req = REP_INFO_OBJ;
-                        memcpy(message.panier, stockage, 
-                                sizeof(message.panier));
-
-                        message.ret = 0;
-                        msgsnd(id_msg, (void *)&message, MSG_SIZE, 0);
-
-                        sprintf(tmpLog, "cli_%d <---[Fournir le stock et prix]--- REP_INFO_OBJ\n",
-                                message.num_clt);
-                        puts(tmpLog);
-                        memset(tmpLog, 0, sizeof(tmpLog));
-                        break;
+				sousProcessusExiste = true;
+				puts("Main task:");
+				traiteMessage(&message, &nb_client, clientPanier, &num_client, false);
+				if (message.type == REP_NUM_CLT)
+				{
+					clientTable[0] = false;
+				} else{
+					clientTable[message.num_clt] = false;
 				}
 			}
 		}
